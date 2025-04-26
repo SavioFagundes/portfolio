a887ec1114ca173e351da416ae324844ec23f5f3
SavioFagundes <akirasfm@gmail.com>
2025-04-26 14:00:55 -0300

    Adicionando classe hoverchangedescription no main

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+
+
+hoverChangeDescription(
+    ".html",
+    "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+  );
+  hoverChangeDescription(
+    ".css",
+    "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+  );
+  hoverChangeDescription(
+    ".js",
+    "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+  );
+  hoverChangeDescription(
+    ".sass",
+    "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+  );
+  hoverChangeDescription(
+    ".react",
+    "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+  );
+  hoverChangeDescription(
+    ".csharp",
+    "C# é uma linguagem de programação moderna e orientada a objetos que permite o desenvolvimento de aplicações robustas, escaláveis e multiplataforma, sendo amplamente usada para construir APIs, sistemas desktop, web e mobile."
+  );
+  hoverChangeDescription(
+    ".angular",
+    "Angular é uma estrutura de desenvolvimento web que permite criar aplicações dinâmicas, responsivas e escaláveis, utilizando TypeScript para construir interfaces modernas e funcionalidades completas no frontend."
+  );
+  hoverChangeDescription(
+    ".tailwind",
+    "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+  );
+  hoverChangeDescription(
+    ".typescript",
+    "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+  );
+  hoverChangeDescription(
+    ".bootstrap",
+    "Bootstrap é um framework de desenvolvimento front-end que permite criar sites e aplicações web responsivos com rapidez, oferecendo componentes prontos e um sistema de grid flexível baseado em HTML, CSS e JavaScript."
+  );
+  hoverChangeDescription(
+    ".python",
+    "Python é uma linguagem de programação de alto nível que permite o desenvolvimento rápido de aplicações web, automações, análise de dados, inteligência artificial e diversas outras soluções, com uma sintaxe simples e poderosa."
+  );
+  hoverChangeDescription(
+    ".dotnet",
+    ".NET Core é uma plataforma de desenvolvimento open-source e multiplataforma que permite criar aplicações web, APIs, microserviços e sistemas de alta performance, utilizando a linguagem C# de forma moderna e escalável."
+  );
